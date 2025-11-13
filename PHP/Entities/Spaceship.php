f3fce5186c02dc98eeb87c8fefba0a98a86e6ed1
Younes013 <40214142@ogt013.nl>
2025-11-13 12:10:36 +0100

    Code bewerkt

--- PHP/Entities/Spaceship.php
+++ PHP/Entities/Spaceship.php
@@ -3,15 +3,11 @@ class Spaceship
 {
 
 public string $Name;
-
 public int $Length;
-
 public int $HP;
-
 public int $Attack;
 
 // 
-
 public function __construct(string $Name, int $Length, int $HP, int $Attack)
 {
    $this->Name = $Name;
@@ -21,55 +17,168 @@ public function __construct(string $Name, int $Length, int $HP, int $Attack)
 }
 
 // 
-
 public function __getName() : string
 {
    return $this->Name;
 }
 
 // 
-
 public function __setName(string $Name) : void{
    $this->Name = $Name;
 }
 
+public function __getLength() : int
+{
+   return $this->Length;
+}
+
+// 
+public function __setLength(int $Length) : void{
+   $this->Length = $Length;
+}
+
+public function __getHP() : int
+{
+   return $this->HP;
+}
+
+// 
+public function __setHP(int $HP) : void{
+   $this->HP = $HP;
+}
+
+public function __getAttack() : int
+{
+   return $this->Attack;
+}
+
+// 
+public function __setAttack(int $Attack) : void{
+   $this->Attack = $Attack;
+}
+}
 
 
-public function __getLength() : string
+
+
+class EnemySpaceship
+{
+
+public string $Name;
+public int $Length;
+public int $HP;
+public int $Attack;
+
+// 
+public function __construct(string $Name, int $Length, int $HP, int $Attack)
+{
+   $this->Name = $Name;
+   $this->Length = $Length;
+   $this->HP = $HP;
+   $this->Attack = $Attack;
+}
+
+// 
+public function __getName() : string
 {
    return $this->Name;
 }
 
 // 
-
-public function __setLength(string $Name) : void{
+public function __setName(string $Name) : void{
    $this->Name = $Name;
+}
+
+public function __getLength() : int
+{
+   return $this->Length;
+}
+
+// 
+public function __setLength(int $Length) : void{
+   $this->Length = $Length;
+}
+
+public function __getHP() : int
+{
+   return $this->HP;
+}
+
+// 
+public function __setHP(int $HP) : void{
+   $this->HP = $HP;
+}
+
+public function __getAttack() : int
+{
+   return $this->Attack;
+}
+
+// 
+public function __setAttack(int $Attack) : void{
+   $this->Attack = $Attack;
+}
 }
 
 
 
-public function __getHP() : string
+
+class Asteroid
+{
+
+public string $Name;
+public int $Length;
+public int $HP;
+public int $Attack;
+
+// 
+public function __construct(string $Name, int $Length, int $HP, int $Attack)
+{
+   $this->Name = $Name;
+   $this->Length = $Length;
+   $this->HP = $HP;
+   $this->Attack = $Attack;
+}
+
+// 
+public function __getName() : string
 {
    return $this->Name;
 }
 
 // 
-
-public function __setHP(string $Name) : void{
+public function __setName(string $Name) : void{
    $this->Name = $Name;
 }
 
-
-
-public function __getAttack() : string
+public function __getLength() : int
 {
-   return $this->Name;
+   return $this->Length;
 }
 
 // 
+public function __setLength(int $Length) : void{
+   $this->Length = $Length;
+}
 
-public function __setAttack(string $Name) : void{
-   $this->Name = $Name;
+public function __getHP() : int
+{
+   return $this->HP;
+}
+
+// 
+public function __setHP(int $HP) : void{
+   $this->HP = $HP;
+}
+
+public function __getAttack() : int
+{
+   return $this->Attack;
+}
+
+// 
+public function __setAttack(int $Attack) : void{
+   $this->Attack = $Attack;
 }
 }
 
--- PHP/index.php
+++ PHP/index.php
@@ -1,27 +1,59 @@
 <?php 
 require_once 'Entities/Spaceship.php';
+require_once 'Entities/EnemySpaceship.php';
+require_once 'Entities/Asteroid.php';
 
 
-$Spaceship = new Spaceship("Destroyer",  5, 6, 2);
-echo($Spaceship->__getName() );
-
-
+// 🛰️ Normal Spaceship
+$Spaceship = new Spaceship("Destroyer", 5, 6, 2);
+echo($Spaceship->__getName());
 
 $Spaceship->__setName(Name:"Cruiser");
 echo("\n{$Spaceship->__getName()}");
 
-
-
-$Spaceship->__setLength(Name:"5");
+$Spaceship->__setLength(Length:"5");
 echo("\n{$Spaceship->__getLength()}");
 
-
-
-$Spaceship->__setHP(Name:"6");
+$Spaceship->__setHP(HP:"6");
 echo("\n{$Spaceship->__getHP()}");
 
+$Spaceship->__setAttack(Attack:"2");
+echo("\n{$Spaceship->__getAttack()}");
 
 
-$Spaceship->__setAttack(Name:"2");
-echo("\n{$Spaceship->__getAttack()}");  
+
+// 👾 Enemy Spaceship
+$EnemySpaceship = new EnemySpaceship("Alien Destroyer", 10, 8, 4);
+echo("\n\n{$EnemySpaceship->__getName()}");
+
+$EnemySpaceship->__setName(Name:"Alien Cruiser");
+echo("\n{$EnemySpaceship->__getName()}");
+
+$EnemySpaceship->__setLength(Length:"10");
+echo("\n{$EnemySpaceship->__getLength()}");
+
+$EnemySpaceship->__setHP(HP:"8");
+echo("\n{$EnemySpaceship->__getHP()}");
+
+$EnemySpaceship->__setAttack(Attack:"4");
+echo("\n{$EnemySpaceship->__getAttack()}");
+
+
+
+// ☄️ Asteroid
+$Asteroid = new Asteroid("Rocky", 15, 20, 0);
+echo("\n\n{$Asteroid->__getName()}");
+
+$Asteroid->__setName(Name:"Big Rock");
+echo("\n{$Asteroid->__getName()}");
+
+$Asteroid->__setLength(Length:"15");
+echo("\n{$Asteroid->__getLength()}");
+
+$Asteroid->__setHP(HP:"20");
+echo("\n{$Asteroid->__getHP()}");
+
+$Asteroid->__setAttack(Attack:"0");
+echo("\n{$Asteroid->__getAttack()}");
+
 ?>
